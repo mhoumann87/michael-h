4de67c571cab1df5c259eeadf27a5a72d80efd31
Michael Houmann <mhoumann87@gmail.com>
2022-04-05 10:57:50 +0200

    Add shared header template and update index.php to use it :Sparkles:

---
+++ private/shared/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Michael Houmann - Web Developer</title>
+</head>
+
+<body>
+
+<header>
+  <div class="logo">
+    <h1>michael-h.dk</h1>
+  </div>
+
+  <nav>
+    <h2>Navigation</h2>
+  </nav>
+</header>
+
+<main>
