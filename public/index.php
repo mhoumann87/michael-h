1710e623e6963d37948df7491de267bce00f20c3
Michael Houmann <mhoumann87@gmail.com>
2022-04-05 13:34:09 +0200

    Update index.php :sparkles:

--- public/index.php
+++ public/index.php
@@ -1,5 +1,11 @@
 <?php
-  include_once '../private/shared/header.php';
+
+$page_title = '- Front Page';
+
+  require_once '../private/initialize.php';
+
+  include '../private/shared/header.php';
+ 
 ?>
 
 </body>
