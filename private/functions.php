64268e66f642073dc73749f880eb091674ed99b5
Michael Houmann <mhoumann87@gmail.com>
2022-04-05 12:50:46 +0200

    Add PHP functions file :sparkles:

---
+++ private/functions.php
@@ -0,0 +1,11 @@
+<?php
+
+//* Set URL for the root of the server
+function url_for($script_path)
+{
+  //* add the leading '/' if it is not present
+  if ($script_path[0] != '/') {
+    $script_path = '/'.$script_path;
+  }
+  return WWW_ROOT.$script_path;
+}
