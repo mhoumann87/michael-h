1a8e42538838e4d1159d13f3c74e57ca83943541
Michael Houmann <mhoumann87@gmail.com>
2022-04-13 10:36:12 +0200

    Add function to validate email format :sparkles:

--- private/validation_functions.php
+++ private/validation_functions.php
@@ -96,3 +96,17 @@ function has_unique_entries($column, $value, $current_id = "0")
     return false;
   }
 }
+
+/*
+ * has_valid_email_format('test@testing.com')
+ * - validate correct format for email addresses
+ * - format [chars]@[chars].[2+ letters]
+ * - preg_match() is helpful, uses a regular expression
+ * - returns 1 for match, 0 for no match
+ * - http://php.net/manual/en/function.preg-match.php 
+*/
+function has_valid_email_format($value) 
+{
+  $email_regex = '/\A[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\Z/i';
+  return preg_match($email_regex, $value) === 1;
+}
