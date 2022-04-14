9d39eae9e86c36ff17c9c90fc20e36fdc158a040
Michael Houmann <mhoumann87@gmail.com>
2022-04-14 08:07:36 +0200

    Add partial for form fields and update new.php :sparkles:

--- public/admin/users/new.php
+++ public/admin/users/new.php
@@ -4,9 +4,12 @@ require_once '../../../private/initialize.php';
 
 $page_title = ' - Create User';
 
-include SHARED_PATH.'/admin_header.php';
+$user = new User;
+
 
 ?>
+
+<?php include SHARED_PATH.'/admin_header.php'; ?>
 
 
 <h2>Create User</h2>
@@ -14,6 +17,16 @@ include SHARED_PATH.'/admin_header.php';
 <aside>
   <?php include './navigation.php'; ?>
 </aside>
+
+<main>
+
+<form action="<?php echo url_for('/admin/users/new.php'); ?>" method="post">
+
+  <?php include './form_fields.php'; ?>
+
+  <input class="btn" type="submit" value="Create User" />
+</form>
+</main>
 
 
 <?php include SHARED_PATH.'/footer.php'; ?>
