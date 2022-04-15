afe173527aa5ed2bccb559a2fac9ebcda138b068
Michael Houmann <mhoumann87@gmail.com>
2022-04-15 14:20:38 +0200

    Update new.php with function to insert to db :sparkles:

--- public/admin/users/new.php
+++ public/admin/users/new.php
@@ -4,8 +4,29 @@ require_once '../../../private/initialize.php';
 
 $page_title = ' - Create User';
 
-$user = new User;
+if (is_post_request()) {
+  // If the form data is submitted, create record based on these parameters
+  
+  // "Collect" all the parameters in an array
+  $args = $_POST['user'];
+  $user = new User($args);
+  $result = $user->save();
 
+  // If we get an result, it means we succeeded and we let the user know
+  // with session_message() and redirect the user to the user page
+  if ($result === true) {
+    $new_id = $user->id;
+    $session->message('User was created successfully.');
+    redirect_to(url_for('/admin/users/show.php?id='.$new_id));
+  }
+
+  // If we get a result that are false, we just automatically show the errors.
+
+} else {
+
+  // The form hasn't been filled and saved yet, so we show an empty form
+  $user = new User;
+}
 
 ?>
 
@@ -19,6 +40,8 @@ $user = new User;
 </aside>
 
 <main>
+
+<?php echo display_errors($user->errors); ?>
 
 <form action="<?php echo url_for('/admin/users/new.php'); ?>" method="post">
 
