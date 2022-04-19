cd1449b34852184c1b62fefaee4a3e7bb1e26555
Michael Houmann <mhoumann87@gmail.com>
2022-04-19 11:18:39 +0200

    Update admin navigation :sparkles:

--- private/shared/admin_nav.php
+++ private/shared/admin_nav.php
@@ -12,20 +12,29 @@
       <a href="<?php echo url_for('/admin/login.php');?>">Login</a>
     </li>
 
+    <li>
+      <a href="<?php echo url_for('/admin/users/create.php'); ?>">Sign Up</a>
+    </li>
+
   <?php } else { ?>
+
+    <li>
+      <a href="<?php echo url_for('/admin/index.php'); ?>">Home</a>
+    </li>
+
+    <li>
+      <a href="<?php echo url_for('/admin/news/index.php'); ?>">News</a>
+    </li>
+
+    <li>
+      <a href="<?php echo url_for('/admin/users/index.php'); ?>">Users</a>
+    </li>
+
     <li>
       <a href="<?php echo url_for('/admin/logout.php'); ?>">Logout</a>
     </li>
   <?php } ?>
 
-    <li>
-      <a href="<?php echo url_for('/admin/users/create.php'); ?>">Sign Up</a>
-    </li>
   </ul>
 </nav>
 
-<?php
-echo $session->is_admin() ? 'True' : 'False';
-echo '<br>';
-  echo user_admin() ? 'True' : 'False';
-  ?>
