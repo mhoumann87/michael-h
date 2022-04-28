c9e8ea0afe545fe589b8dec13c31f3e5daf8da49
Michael Houmann <mhoumann87@gmail.com>
2022-04-28 12:02:29 +0200

    Change fonts links :sparkles:

--- private/shared/admin_header.php
+++ private/shared/admin_header.php
@@ -16,7 +16,7 @@ if (!isset($page_title)) {
 
   <link rel="preconnect" href="https://fonts.googleapis.com" />
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&family=Poiret+One&display=swap" rel="stylesheet" />
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400&family=PT+Serif:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet" />
 
   <link rel="stylesheet" href="<?php echo url_for('/assets/css/styles.css'); ?>">
 </head>
--- private/shared/header.php
+++ private/shared/header.php
@@ -17,8 +17,8 @@
   <title>michael-h.dk <?php echo $page_title; ?></title>
 
   <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&family=Staatliches&display=swap" rel="stylesheet" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400&family=PT+Serif:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet" />
 
   <link rel="stylesheet" href="<?php echo url_for('/assets/css/styles.css'); ?>">
 </head>
