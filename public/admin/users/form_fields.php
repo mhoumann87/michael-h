63453d4babdbc9091e48c476be01bb93d93a965a
Michael Houmann <mhoumann87@gmail.com>
2022-05-02 11:05:44 +0200

    Add option to change role for user if admin :sparkles:

--- public/admin/users/form_fields.php
+++ public/admin/users/form_fields.php
@@ -9,13 +9,41 @@ if (!isset($user)) {
 ?>
 
 <label for="username">Username: </label>
-<input type="text" name="user[username]" value="<?php echo h($user->username); ?>" />
+<input 
+  type="text" 
+  name="user[username]" 
+  value="<?php echo h($user->username); ?>" 
+  id="username" />
 
 <label for="email">Email: </label>
-<input type="text" name="user[email]" value="<?php echo h($user->email); ?>" />
+<input 
+  type="text" 
+  name="user[email]" 
+  value="<?php echo h($user->email); ?>" 
+  id="email" />
 
 <label for="password">Password: </label>
-<input type="password" name="user[password]" />
+<input type="password" name="user[password]" id="password" />
 
 <label for="confirm_password">Confirm Password: </label>
-<input type="password" name="user[confirm_password]" />
+<input type="password" name="user[confirm_password]" id="confirm_password" />
+
+<?php if (is_admin()) { ?>
+  <label for="role">Users Role</label>
+  
+  <input 
+    type="radio" 
+    name="user[is_admin]" 
+    value="0" 
+    <?php echo ($user->is_admin == 0) ? 'checked="checked"' : ''; ?> 
+    id="role" />
+    User &nbsp;
+
+  <input 
+    type="radio" 
+    name="user[is_admin]" 
+    value="1"
+    <?php echo ($user->is_admin > 0) ? 'checked="checked"' : ''; ?> 
+    id="role" />Administrator &nbsp;
+    
+<?php } ?>
