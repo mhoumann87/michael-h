8bb3e3e37c347f6ddc381415b47a8c78dc3de4b2
Michael Houmann <mhoumann87@gmail.com>
2022-05-09 09:36:43 +0200

    Move navigation to header :truck:

--- private/shared/header.php
+++ private/shared/header.php
@@ -33,11 +33,33 @@ if (!isset($page_title)) {
         <img src="<?php echo url_for('/assets/images/logo.svg'); ?>" alt="logo" />
       </div>
 
-      <nav>
 
-        <?php include SHARED_PATH . '/nav.php';?>
+<button
+  class="mobile-nav-toggle"
+  aria-controls="primary-navigation"
+  aria-expanded="false"
+  >
+  <span class="sr-only">Menu</span>
+</button>
 
-      </nav>
+<nav role="primary-navigation">
+
+  <ul
+    id="primary-navigation"
+    class="flex primary-navigation"
+    data-visible="false"
+    >
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Portfolio</a></li>
+    <li><a href="#">News</a></li>
+    <li><a href="#">Photography</a></li>
+    <li><a href="#">About</a></li>
+    <li><a href="#">Contact</a></li>
+
+  </ul>
+
+</nav>
+
 
     </section>
 
