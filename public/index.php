aea348c3f1f247b44ed1d41e4a652772dd606816
Michael Houmann <mhoumann87@gmail.com>
2022-05-10 10:57:31 +0200

    Update page for responsive menu :sparkles:

--- public/index.php
+++ public/index.php
@@ -10,23 +10,32 @@ include '../private/shared/header.php';
 
 <main>
 
-<section class="hero">
+  <section class="hero">
 
-<div class="container">
+    <div class="container">
 
-  <h1>This is a simple test front page</h1>
-  </div>
+      <h1>This is a simple test front page</h1>
+
+    </div>
+
   </section>
-  <section class="content grid content-grid">
 
-    <section class="container">
+  <section class="front-page content grid content-grid container">
 
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, dolores! Deleniti sit, aliquam inventore veniam corporis blanditiis. Cupiditate molestias iure provident rerum voluptatum aliquid tenetur similique animi assumenda, tempora ratione aliquam odit vel magni architecto adipisci facere eos omnis amet alias iusto aperiam! Aut necessitatibus repellat vel eius, molestiae consequatur.
+    <aside class="news">
+      <h3>News</h3>
+    </aside>
+
+    <section>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, dolores! Deleniti sit, aliquam inventore veniam corporis blanditiis. Cupiditate molestias iure provident rerum voluptatum aliquid tenetur similique animi assumenda, tempora ratione aliquam odit vel magni architecto adipisci facere eos omnis amet alias iusto aperiam! Aut necessitatibus repellat vel eius, molestiae consequatur.
+      </p>
 
     </section>
 
-
   </section>
+
 </main>
 
 <?php include '../private/shared/footer.php';?>
