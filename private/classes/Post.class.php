d88d5a175e8d32a852849d3f9b123ba6a3f48037
Michael Houmann <mhoumann87@gmail.com>
2022-05-19 14:03:29 +0200

    Add class and start code :sparkles:

---
+++ private/classes/Post.class.php
@@ -0,0 +1,55 @@
+<?php
+
+class Post extends DatabaseObject
+{
+
+    protected static $table_name = 'posts';
+    protected static $db_columns = [
+        'post_id',
+        'author_id',
+        'image_id',
+        'headline',
+        'content',
+        'created',
+        'updated',
+        'is_published',
+    ];
+
+    public $allowed_tags = [
+        '<br>',
+        '<a>',
+        '<p>',
+        '<h3>',
+        '<h4>',
+        '<span>',
+    ];
+
+    public $post_id;
+    public $author_id;
+    public $image_id;
+    public $headline;
+    public $content;
+    public $is_published;
+    protected $created;
+    protected $published;
+
+    public function __construct($args = [])
+    {
+        $this->author_id = $args['author_id'] ?? '';
+        $this->image_id = $args['image_id'] ?? '';
+        $this->headline = $args['headline'] ?? '';
+        $this->content = $args['content'] ?? '';
+        $this->is_published = $args['is_published'] ?? '';
+    }
+
+    public function set_created()
+    {
+        $this->created = time();
+    }
+
+    public function set_updated()
+    {
+        $this->updated = time();
+    }
+
+} // end Post
