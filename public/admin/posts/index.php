db15f5ca92901ff48e0fc6e387f607a3f77aad97
Michael Houmann <mhoumann87@gmail.com>
2022-05-20 10:57:47 +0200

    Add starter files for posts front-end :sparkles:

---
+++ public/admin/posts/index.php
@@ -0,0 +1,33 @@
+<?php require_once '../../../private/initialize.php';?>
+
+<?php
+
+$title = is_admin() ? 'All Posts' : 'My Posts';
+
+$page_title = ' - ' . $title;
+
+?>
+
+<?php include SHARED_PATH . '/admin_header.php';?>
+
+<main class="container">
+
+  <h2>Hastily made news page</h2>
+
+  <section class="grid content-grid">
+
+    <aside>
+
+      <?php include '../navigation.php';?>
+
+    </aside>
+
+    <section class="content">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate perferendis magnam non. Sed dolorem neque laboriosam doloremque ut nisi dolorum quaerat commodi quae fugiat, sit laborum tenetur rem quis consequuntur error ipsam, velit excepturi iure quo, eveniet numquam. Blanditiis repudiandae quae accusamus enim rem hic itaque porro debitis aliquid animi!
+    </section>
+
+  </section><!-- grid content-grid -->
+
+</main>
+
+<?php include SHARED_PATH . '/footer.php';?>
