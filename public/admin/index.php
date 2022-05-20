1fe1a944daa3c7257b3cb1a2e627a3f9261ac367
Michael Houmann <mhoumann87@gmail.com>
2022-05-20 11:03:06 +0200

    Remove navigation form sidebar and refer to seperate file :truck:

--- public/admin/index.php
+++ public/admin/index.php
@@ -5,56 +5,19 @@ require_login();
 
 $page_title = ' - Home';
 
-include_once SHARED_PATH.'/admin_header.php';
+include SHARED_PATH . '/admin_header.php';
 ?>
 
-<main>
+<main class="container">
 
   <h2>Admin Area</h2>
 
   <section class="grid content-grid">
 
     <aside>
-      <nav class="sidebar-nav" role="secondary-navigation">
-        
-        <ul role="list">
-        
-          <li><a href="#">My Posts</a></li>
-        
-          <li><a href="#">Create Post</a></li>
-        
-        <?php if (is_admin()) { ?>
-          <li><a href="<?php echo url_for('/admin/news/index.php'); ?>">
-            All Posts
-          </a></li>  
-        <?php } ?>
-        
-          <li>
-            <a href="<?php echo url_for('/admin/users/show.php'); ?> ">
-            My Profile
-            </a>
-          </li>
 
-        <?php if(is_admin()) { ?>
-        
-          <li>
-            <a href="<?php echo url_for('/admin/users/new.php'); ?>">
-            Create User
-            </a>
-          </li>
+      <?php include './navigation.php';?>
 
-        
-          <li>
-            <a href="<?php echo url_for('/admin/users/index.php'); ?>">
-            All Users
-            </a>
-          </li>
-        <?php } ?>
-
-        </ul>
-      
-      </nav>
-    
     </aside>
 
     <section>
@@ -63,6 +26,6 @@ include_once SHARED_PATH.'/admin_header.php';
 
   </section>
 
-  <main>
+</main>
 
-<?php include_once SHARED_PATH.'/footer.php'; ?>
+<?php include SHARED_PATH . '/footer.php';?>
